e45d5488d998da7f0ded717c0ca30de6fa029484
joeyak <joeyak@umich.edu>
2015-02-06 02:19:46 -0500

    2nd commit

--- main/src/Greeter.java
+++ main/src/Greeter.java
@@ -1,11 +1,19 @@
 /**
- * Created by Joe Korany on 2/6/2015.
+ * A class for producing simple greetings. Created by Joe Korany on 2/6/2015.
  */
 public class Greeter {
+    /**
+     * Constructs a Greeter object that can greet a person or entity.
+     * @param aName the name of the person or entity.
+     */
     public Greeter(String aName) {
         name = aName;
     }
 
+    /**
+     * Greets with a "Hello" message.
+     * @return a message greeting the person.
+     */
     public String sayHello() {
         return "Hello, " + name + "!";
     }
--- main/src/GreeterTest.java
+++ main/src/GreeterTest.java
@@ -6,5 +6,20 @@ public class GreeterTest {
         Greeter worldGreeter = new Greeter("World");
         String greeting = worldGreeter.sayHello();
         System.out.println(greeting);
+        Greeter newGreeter = null;
+        try {
+            newGreeter.sayHello();
+        }
+        catch(Exception e) {
+            System.out.println("Exception : " + e.getMessage());
+        }
+        String test = "Test";
+        String s = test;
+        s = s + " does this work?";
+        System.out.println(s);
+        System.out.println(test);
+        
     }
+
 }
+
